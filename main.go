1c6c923696578c5ad9f5f162cc0fafd6732b90b3
mGGk <mggk@protonmail.ch>
2018-08-27 17:21:48 +0200

    html index + total ram

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
-package gocade
+package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/capnm/sysinfo"
 )
 
+func httpRAMUsage(w http.ResponseWriter, r *http.Request) {
+	si := sysinfo.Get()
+	w.Write([]byte(fmt.Sprintf("%d", si.TotalRam)))
+}
+
 func main() {
-	fmt.Print("goCade v0.1")
+	fmt.Println("goCade v0.1")
+	fmt.Println("Starting web server and defining routes")
+	http.Handle("/", http.FileServer(http.Dir("./pages")))
+	http.HandleFunc("/getRAMUsage", httpRAMUsage)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }

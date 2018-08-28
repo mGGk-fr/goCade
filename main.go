3367e6e041510ea9a0067c6ce0ebf93e1196f04a
mGGk <mggk@protonmail.ch>
2018-08-28 17:47:28 +0200

    using proc/meminfo for memory infos

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/capnm/sysinfo"
+	"os/exec"
 )
 
 func httpRAMUsage(w http.ResponseWriter, r *http.Request) {
-	si := sysinfo.Get()
-	w.Write([]byte(fmt.Sprintf("%d", si.TotalRam)))
+	out, err := exec.Command("sh", "-c", " awk '/MemAvailable/ { printf \"%.3f\", $2/1024/1024 }' /proc/meminfo").Output()
+	if err != nil {
+		panic(err)
+	}
+	w.Write(out)
+}
+
+func httpRAMTotal(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("sh", "-c", " awk '/MemTotal/ { printf \"%.3f\", $2/1024/1024 }' /proc/meminfo").Output()
+	if err != nil {
+		panic(err)
+	}
+	w.Write(out)
 }
 
 func main() {
@@ -17,6 +27,7 @@ func main() {
 	fmt.Println("Starting web server and defining routes")
 	http.Handle("/", http.FileServer(http.Dir("./pages")))
 	http.HandleFunc("/getRAMUsage", httpRAMUsage)
+	http.HandleFunc("/getRAMTotal", httpRAMTotal)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}

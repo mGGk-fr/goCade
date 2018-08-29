b309093f5b1b63ef34c203abbe9afb2966ec66fd
mGGk <mggk@protonmail.ch>
2018-08-29 12:19:54 +0200

    getting cpu usage in percent

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"runtime"
 )
 
 func httpRAMUsage(w http.ResponseWriter, r *http.Request) {
@@ -22,12 +23,21 @@ func httpRAMTotal(w http.ResponseWriter, r *http.Request) {
 	w.Write(out)
 }
 
+func httpCPUUsage(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("sh", "-c", "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'").Output()
+	if err != nil {
+		panic(err)
+	}
+	w.Write(out)
+}
+
 func main() {
-	fmt.Println("goCade v0.1")
+	fmt.Println("goCade v0.1 running on " + runtime.GOOS)
 	fmt.Println("Starting web server and defining routes")
 	http.Handle("/", http.FileServer(http.Dir("./pages")))
 	http.HandleFunc("/getRAMUsage", httpRAMUsage)
 	http.HandleFunc("/getRAMTotal", httpRAMTotal)
+	http.HandleFunc("/getCPUUsage", httpCPUUsage)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}

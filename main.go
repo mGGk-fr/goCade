fc92a94748877abe554c19bb622b2af9bbb7c403
mGGk <mggk@protonmail.ch>
2018-08-30 14:28:53 +0200

    cleaning code

--- main.go
+++ main.go
@@ -2,33 +2,60 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
 	"runtime"
 )
 
-func httpRAMUsage(w http.ResponseWriter, r *http.Request) {
-	out, err := exec.Command("sh", "-c", " awk '/MemAvailable/ { printf \"%.3f\", $2/1024/1024 }' /proc/meminfo").Output()
+func execUnixSysCommand(c string) []byte {
+	out, err := exec.Command("sh", "-c", c).Output()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
+		return nil
 	}
-	w.Write(out)
+	return out
+}
+
+func execWinSysCommand(c string) []byte {
+	return nil
+}
+
+func httpRAMUsage(w http.ResponseWriter, r *http.Request) {
+	var co []byte
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		co = execUnixSysCommand(" awk '/MemAvailable/ { printf \"%.3f\", $2/1024/1024 }' /proc/meminfo")
+	case "windows":
+		co = execWinSysCommand("")
+	}
+	w.Write(co)
 }
 
 func httpRAMTotal(w http.ResponseWriter, r *http.Request) {
-	out, err := exec.Command("sh", "-c", " awk '/MemTotal/ { printf \"%.3f\", $2/1024/1024 }' /proc/meminfo").Output()
-	if err != nil {
-		panic(err)
+	var co []byte
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		co = execUnixSysCommand(" awk '/MemTotal/ { printf \"%.3f\", $2/1024/1024 }' /proc/meminfo")
+	case "windows":
+		co = execWinSysCommand("")
 	}
-	w.Write(out)
+	w.Write(co)
 }
 
 func httpCPUUsage(w http.ResponseWriter, r *http.Request) {
-	out, err := exec.Command("sh", "-c", "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'").Output()
-	if err != nil {
-		panic(err)
+	var co []byte
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		co = execUnixSysCommand("grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'")
+	case "windows":
+		co = execWinSysCommand("")
 	}
-	w.Write(out)
+	w.Write(co)
+}
+
+func httpCurrentGame(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
@@ -38,6 +65,7 @@ func main() {
 	http.HandleFunc("/getRAMUsage", httpRAMUsage)
 	http.HandleFunc("/getRAMTotal", httpRAMTotal)
 	http.HandleFunc("/getCPUUsage", httpCPUUsage)
+	http.HandleFunc("/getCurrentGame", httpCurrentGame)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
